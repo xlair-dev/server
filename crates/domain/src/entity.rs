726e89bf342d7028272fdcb9f2765911e8983da1
n4mlz <n4mlz.dev@gmail.com>
2025-04-17 20:46:18 +0900

    :sparkles: record を実装

--- crates/domain/src/entity.rs
+++ crates/domain/src/entity.rs
@@ -3,5 +3,6 @@ pub mod difficulty;
 pub mod genre;
 pub mod level;
 pub mod music;
+pub mod record;
 pub mod sheet;
 pub mod user;
---
+++ crates/domain/src/entity/record.rs
@@ -0,0 +1,22 @@
+use chrono::NaiveDateTime;
+use getset::{Getters, Setters};
+
+use super::clear_type::ClearType;
+
+#[derive(Debug, Getters, Setters)]
+pub struct Record {
+    #[getset(get = "pub")]
+    id: String,
+    #[getset(get = "pub")]
+    user_id: String,
+    #[getset(get = "pub")]
+    sheet_id: String,
+    #[getset(get = "pub", set = "pub")]
+    score: u32,
+    #[getset(get = "pub", set = "pub")]
+    clear_type: ClearType,
+    #[getset(get = "pub", set = "pub")]
+    play_count: u32,
+    #[getset(get = "pub", set = "pub")]
+    updated_at: NaiveDateTime, // TODO: consider using DateTime<Utc>
+}
