942a1a966237d0019629193bb7194d630e19cf81
n4mlz <n4mlz.dev@gmail.com>
2025-04-17 20:41:30 +0900

    :adhesive_bandage: enum で実装するように変更

--- crates/domain/src/entity/difficulty.rs
+++ crates/domain/src/entity/difficulty.rs
@@ -1,37 +1,19 @@
-use thiserror::Error;
+use std::fmt::{Display, Formatter, Result};
 
-const DIFFICULTY_NAMES: [&str; 3] = [
-    // TODO: Determine the difficulty names
-    "EASY", "NORMAL", "HARD",
-];
-
+// TODO: Determine the difficulty names
 #[derive(Debug)]
-pub struct Difficulty(u32);
-
-#[derive(Debug, Error)]
-pub enum DifficultyError {
-    #[error("Invalid difficulty value")]
-    InvalidValue,
+pub enum Difficulty {
+    Easy,
+    Normal,
+    Hard,
 }
 
-impl TryFrom<u32> for Difficulty {
-    type Error = DifficultyError;
-
-    fn try_from(value: u32) -> Result<Self, Self::Error> {
-        if !(0..DIFFICULTY_NAMES.len() as u32).contains(&value) {
-            return Err(DifficultyError::InvalidValue);
+impl Display for Difficulty {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Difficulty::Easy => write!(f, "EASY"),
+            Difficulty::Normal => write!(f, "NORMAL"),
+            Difficulty::Hard => write!(f, "HARD"),
         }
-
-        Ok(Difficulty(value))
-    }
-}
-
-impl Difficulty {
-    pub fn value(&self) -> u32 {
-        self.0
-    }
-
-    pub fn name(&self) -> &str {
-        DIFFICULTY_NAMES[self.0 as usize]
     }
 }
--- crates/domain/src/entity/genre.rs
+++ crates/domain/src/entity/genre.rs
@@ -1,37 +1,15 @@
-use thiserror::Error;
+use std::fmt::{Display, Formatter, Result};
 
-const GENRE_NAMES: [&str; 1] = [
-    "ORIGINAL",
-    // TODO: Add the rest of the genres
-];
-
+// TODO: Add tests for the Genre struct
 #[derive(Debug)]
-pub struct Genre(u32);
-
-#[derive(Debug, Error)]
-pub enum GenreError {
-    #[error("Invalid genre value")]
-    InvalidValue,
+pub enum Genre {
+    ORIGINAL,
 }
 
-impl TryFrom<u32> for Genre {
-    type Error = GenreError;
-
-    fn try_from(value: u32) -> Result<Self, Self::Error> {
-        if !(0..GENRE_NAMES.len() as u32).contains(&value) {
-            return Err(GenreError::InvalidValue);
+impl Display for Genre {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Genre::ORIGINAL => write!(f, "ORIGINAL"),
         }
-
-        Ok(Genre(value))
-    }
-}
-
-impl Genre {
-    pub fn value(&self) -> u32 {
-        self.0
-    }
-
-    pub fn name(&self) -> &str {
-        GENRE_NAMES[self.0 as usize]
     }
 }
