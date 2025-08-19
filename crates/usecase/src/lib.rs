f448ffac91538c2b8f49d7d1760efe6b8332223a
n4mlz <n4mlz.dev@gmail.com>
2025-08-19 22:10:15 +0900

    :recycle: refactor

--- crates/usecase/src/lib.rs
+++ crates/usecase/src/lib.rs
@@ -1,1 +1,5 @@
 pub mod user;
+
+pub struct Usecases<R: domain::repository::Repositories> {
+    pub user: user::UserUsecase<R>,
+}
--- crates/usecase/src/user/register.rs
+++ crates/usecase/src/user/register.rs
@@ -1,14 +1,11 @@
-use domain::{
-    entity::user::User,
-    repository::{user::UserRepository, Repositories},
-};
+use domain::{entity::user::User, repository::Repositories};
 
 use crate::user::{dto::RegisterUserDto, UserUsecase, UserUsecaseError};
 
 impl<R: Repositories> UserUsecase<R> {
     pub fn register(&self, raw_user: RegisterUserDto) -> Result<(), UserUsecaseError> {
         let user = User::new_temporary(raw_user.card, raw_user.display_name);
-        self.repositories.user_repository().create(user)?;
+        self.repositories.user().create(user)?;
         Ok(())
     }
 }
