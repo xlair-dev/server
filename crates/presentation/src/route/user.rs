509cbbaa1821fc6b234354e6a002b716762321c2
n4mlz <n4mlz.dev@gmail.com>
2025-08-20 00:01:38 +0900

    :recycle: よりシンプルに

--- crates/presentation/src/route/user.rs
+++ crates/presentation/src/route/user.rs
@@ -1,25 +1,16 @@
 use axum::{extract::State, http::StatusCode, Json};
 
-use crate::model::user::{RegisterUserRequest, UserDataResponse};
+use crate::{
+    error::AppError,
+    model::user::{RegisterUserRequest, UserDataResponse},
+};
+
+type AppResult<T> = Result<T, AppError>;
 
 pub async fn handle_post(
     State(state): State<crate::state::State>,
     Json(req): Json<RegisterUserRequest>,
-) -> (StatusCode, Json<UserDataResponse>) {
-    match state.usecases.user.register(req.into()) {
-        Ok(user_data) => {
-            let user_data = user_data.into();
-            (StatusCode::CREATED, Json(user_data))
-        }
-        Err(_) => {
-            // TODO: エラーごとに適切なレスポンスを返す
-            let user_data = UserDataResponse {
-                id: "".to_string(),
-                card: req.card,
-                display_name: req.display_name,
-                credits: 0,
-            };
-            (StatusCode::INTERNAL_SERVER_ERROR, Json(user_data))
-        }
-    }
+) -> AppResult<(StatusCode, Json<UserDataResponse>)> {
+    let user_data = state.usecases.user.register(req.into())?;
+    Ok((StatusCode::CREATED, Json(user_data.into())))
 }
