9ee50966719bba673109515cbeec597bba026cb6
n4mlz <n4mlz.dev@gmail.com>
2025-08-19 23:38:08 +0900

    :sparkles: API を一つ作成

--- crates/presentation/src/route/user.rs
+++ crates/presentation/src/route/user.rs
@@ -1,5 +1,25 @@
-use axum::extract::State;
+use axum::{extract::State, http::StatusCode, Json};
 
-pub async fn handle_post(State(state): State<crate::state::State>) -> String {
-    "User created successfully".to_string()
+use crate::model::user::{RegisterUserRequest, UserDataResponse};
+
+pub async fn handle_post(
+    State(state): State<crate::state::State>,
+    Json(req): Json<RegisterUserRequest>,
+) -> (StatusCode, Json<UserDataResponse>) {
+    match state.usecases.user.register(req.into()) {
+        Ok(user_data) => {
+            let user_data = user_data.into();
+            (StatusCode::CREATED, Json(user_data))
+        }
+        Err(_) => {
+            // TODO: エラーごとに適切なレスポンスを返す
+            let user_data = UserDataResponse {
+                id: "".to_string(),
+                card: req.card,
+                display_name: req.display_name,
+                credits: 0,
+            };
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(user_data))
+        }
+    }
 }
