bc3c8b3b8ff76bcbae0bb4c2405e50f2c94140de
n4mlz <n4mlz.dev@gmail.com>
2025-10-07 16:48:46 +0900

    :gear: 環境変数を追加

--- crates/presentation/src/env.rs
+++ crates/presentation/src/env.rs
@@ -1,9 +1,24 @@
 use std::env;
 
 pub fn host() -> String {
-    env::var("HOST").unwrap_or("127.0.0.1".into())
+    env::var("HOST").expect("HOST must be set")
 }
 
 pub fn port() -> String {
-    env::var("PORT").unwrap_or("8080".into())
+    env::var("PORT").expect("PORT must be set")
+}
+
+pub fn postgres_url() -> String {
+    let user = env::var("POSTGRES_USER").expect("POSTGRES_USER must be set");
+    let password = env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD must be set");
+    let db = env::var("POSTGRES_DB").expect("POSTGRES_DB must be set");
+    let port = env::var("POSTGRES_PORT").expect("POSTGRES_PORT must be set");
+    format!(
+        "postgres://{}:{}@{}:{}/{}",
+        user,
+        password,
+        host(),
+        port,
+        db
+    )
 }
